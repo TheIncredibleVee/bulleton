02306d7e68f39f528d370cca176acdd00edbfc71
TheIncredibleVee <cehvinaysachdeva@gmail.com>
2021-07-16 10:34:37 +0530

    Updates

---
+++ precache-manifest.3df7e6b5c991980cb9a4bc94bc627cd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a781f8eadee19e402d464e2b73a116",
+    "url": "/bulleton/index.html"
+  },
+  {
+    "revision": "cfa3552d54d97fa9da45",
+    "url": "/bulleton/static/css/main.678fe033.chunk.css"
+  },
+  {
+    "revision": "7425808ff16eda7b8662",
+    "url": "/bulleton/static/js/2.b2d75996.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bulleton/static/js/2.b2d75996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa3552d54d97fa9da45",
+    "url": "/bulleton/static/js/main.15bc2d57.chunk.js"
+  },
+  {
+    "revision": "782598f895c9fa1fa95a",
+    "url": "/bulleton/static/js/runtime-main.e335df2d.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/bulleton/static/media/logo.b0a1eb7c.png"
+  }
+]);
